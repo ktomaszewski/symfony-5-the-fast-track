d0d0a2ca91b4b2b1ffaa2538987422d46bd04d03
ktomaszewski <tomaszewski.kamil.97@gmail.com>
2020-05-20 20:20:24 +0200

    Added basic homepage

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return new Response(<<<EOF
+<html>
+    <body>
+        <img src="/images/under-construction.gif"/>
+    </body>
+</html>
+EOF
+        );
+    }
+}
