95ac9b68a1f053606e7fce2957f2386cc1d8173a
ktomaszewski <tomaszewski.kamil.97@gmail.com>
2020-05-22 21:21:16 +0200

    Added list of conferences

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,24 +4,21 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index()
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        return new Response(<<<EOF
-<html>
-    <body>
-        <img src="/images/under-construction.gif"/>
-    </body>
-</html>
-EOF
-        );
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll()
+        ]));
     }
 }
