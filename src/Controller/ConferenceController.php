8abd8c1a06b906a21a0d53e16fd56c4d3707fc19
ktomaszewski <tomaszewski.kamil.97@gmail.com>
2020-05-22 21:53:57 +0200

    a

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ namespace App\Controller;
 use App\Entity\Conference;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\Common\Collections\Criteria;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,7 +32,7 @@ class ConferenceController extends AbstractController
     {
         return new Response($twig->render('conference/show.html.twig', [
             'conference' => $conference,
-            'comments'   => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC'])
+            'comments'   => $commentRepository->findBy(['conference' => $conference], ['createdAt' => Criteria::DESC])
         ]));
     }
 }
