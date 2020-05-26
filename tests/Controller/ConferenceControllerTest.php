41367bfbd35c526e9382a77c3059d2c6f4c22ec6
ktomaszewski <tomaszewski.kamil.97@gmail.com>
2020-05-26 14:57:23 +0200

    Added new functional test

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -17,4 +17,19 @@ final class ConferenceControllerTest extends WebTestCase
         static::assertResponseIsSuccessful();
         static::assertSelectorTextContains('h2', 'Give your feedback');
     }
+
+    public function testConferencePage(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request(Request::METHOD_GET, '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        static::assertPageTitleContains('Amsterdam');
+        static::assertResponseIsSuccessful();
+        static::assertSelectorTextContains('h2', 'Amsterdam 2019');
+        static::assertSelectorExists('div:contains("There are 1 comments")');
+    }
 }
