1e43c6616cc6ad89c47f1ee5004d103a98785ae6
ktomaszewski <tomaszewski.kamil.97@gmail.com>
2020-05-31 15:17:25 +0200

    Added cache support

--- src/Command/StepInfoCommand.php
+++ src/Command/StepInfoCommand.php
@@ -4,20 +4,37 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use Psr\Cache\CacheItemInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class StepInfoCommand extends Command
 {
     protected static $defaultName = 'app:step:info';
 
+    /** @var CacheInterface */
+    private $cache;
+
+    public function __construct(CacheInterface $cache)
+    {
+        parent::__construct();
+
+        $this->cache = $cache;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
-        $process->mustRun();
-        $output->write($process->getOutput());
+        $step = $this->cache->get('app.current_Step', static function (CacheItemInterface $item) {
+            $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
+            $process->mustRun();
+            $item->expiresAfter(30);
+
+            return $process->getOutput();
+        });
+        $output->write($step);
 
         return 0;
     }
