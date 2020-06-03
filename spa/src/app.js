279470c852605b4d0de3aef509f516f815d904d2
ktomaszewski <tomaszewski.kamil.97@gmail.com>
2020-06-03 18:13:18 +0200

    Added Hello World

---
+++ spa/src/app.js
@@ -0,0 +1,11 @@
+import {h, render} from 'preact';
+
+function App() {
+    return (
+        <div>
+            Hello World!
+        </div>
+    );
+}
+
+render(<App/>, document.getElementById('app'));
